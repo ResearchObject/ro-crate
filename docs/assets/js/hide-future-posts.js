61992fc2b4fdc90ad5fb78720cc200da4d54ae1d
Eli Chadwick <eli.chadwick@manchester.ac.uk>
2026-01-14 14:49:51 +0000

    simplify by just not loading hide-future-posts.js if site.future is true

--- docs/assets/js/hide-future-posts.js
+++ docs/assets/js/hide-future-posts.js
@@ -3,8 +3,5 @@ $('li[date]').each(function () {
     // parse the date
     var itemDate = new Date($(this).attr('date') + "T00:00:00Z");
     // compare dates and hide future posts
-    var show_future = document.currentScript.getAttribute("data-show-future");
-    if (show_future != "true") {
-        if (itemDate > Date.now()) $(this).hide();
-    };
+    if (itemDate > Date.now()) $(this).hide();
 });
